ec7e4fc7c461d4cce869cf89ad11fff948e71f61
Garcon2Jeu <104232891+Garcon2Jeu@users.noreply.github.com>
2023-06-15 15:01:11 +0200

    make:entity -> Tutorial

---
+++ src/Entity/Tutorial.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TutorialRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TutorialRepository::class)]
+class Tutorial
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $objective = null;
+
+    #[ORM\Column]
+    private ?bool $isPublished = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $indexOrder = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $picturePath = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $iconPath = null;
+
+    #[ORM\ManyToOne(inversedBy: 'tutorials')]
+    private ?Theme $theme = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getObjective(): ?string
+    {
+        return $this->objective;
+    }
+
+    public function setObjective(?string $objective): self
+    {
+        $this->objective = $objective;
+
+        return $this;
+    }
+
+    public function isIsPublished(): ?bool
+    {
+        return $this->isPublished;
+    }
+
+    public function setIsPublished(bool $isPublished): self
+    {
+        $this->isPublished = $isPublished;
+
+        return $this;
+    }
+
+    public function getIndexOrder(): ?int
+    {
+        return $this->indexOrder;
+    }
+
+    public function setIndexOrder(?int $indexOrder): self
+    {
+        $this->indexOrder = $indexOrder;
+
+        return $this;
+    }
+
+    public function getPicturePath(): ?string
+    {
+        return $this->picturePath;
+    }
+
+    public function setPicturePath(?string $picturePath): self
+    {
+        $this->picturePath = $picturePath;
+
+        return $this;
+    }
+
+    public function getIconPath(): ?string
+    {
+        return $this->iconPath;
+    }
+
+    public function setIconPath(?string $iconPath): self
+    {
+        $this->iconPath = $iconPath;
+
+        return $this;
+    }
+
+    public function getTheme(): ?Theme
+    {
+        return $this->theme;
+    }
+
+    public function setTheme(?Theme $tutorial): self
+    {
+        $this->theme = $tutorial;
+
+        return $this;
+    }
+}
