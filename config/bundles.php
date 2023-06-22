d922f0fd9d08a25b2a579b6b35d0343f93160f76
Garcon2Jeu <104232891+Garcon2Jeu@users.noreply.github.com>
2023-06-22 13:22:57 +0200

    Installed symfony/ux-twig-component

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
 ];
