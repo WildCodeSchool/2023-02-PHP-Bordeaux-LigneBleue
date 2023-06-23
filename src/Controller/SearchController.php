90c706a80bc57bbd77f1ca6499165d2f5f72d455
Garcon2Jeu <104232891+Garcon2Jeu@users.noreply.github.com>
2023-06-23 12:23:06 +0200

    Implemented "separated" filters

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Tag;
+use App\Entity\Theme;
 use App\Repository\TutorialRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -17,6 +20,7 @@ class SearchController extends AbstractController
         $searchForm = $this->createFormBuilder()
             ->add("search", TextType::class, [
                 'label' => false,
+                'csrf_protection' => false,
                 'attr' => [
                     'class' => 'form-control llb_navbar_search_form',
                     'placeholder' => 'Je cherche une formation',
@@ -32,6 +36,8 @@ class SearchController extends AbstractController
 
         if ($searchForm->isSubmitted() && $searchForm->isValid()) {
             $searchData = $searchForm->getData()["search"];
+            $session = $request->getSession();
+            $session->set("searchData", $searchData);
 
             $tutorials = $tutorialRepository->searchTutorials($searchData);
             return $this->render('search/index.html.twig', [
@@ -42,6 +48,60 @@ class SearchController extends AbstractController
 
         return $this->render('search/_form.html.twig', [
             'searchForm' => $searchForm
+        ]);
+    }
+
+    #[Route('/search/category/{category}', name: 'app_search_filter_category')]
+    public function filterByCategory(
+        Request $request,
+        Category $category,
+        TutorialRepository $tutorialRepository
+    ): Response {
+        $session = $request->getSession();
+        $session->set("category", $category->getCategoryTitle());
+        $searchData = $session->get("searchData");
+
+        $tutorials = $tutorialRepository->filterByCategory($searchData, $category->getID());
+
+        return $this->render('search/index.html.twig', [
+            'searchData' => $searchData,
+            'tutorials' => $tutorials
+        ]);
+    }
+
+    #[Route('/search/theme/{theme}', name: 'app_search_filter_theme')]
+    public function filterByTheme(
+        Request $request,
+        Theme $theme,
+        TutorialRepository $tutorialRepository
+    ): Response {
+        $session = $request->getSession();
+        $session->set("theme", $theme->getTitle());
+        $searchData = $session->get("searchData");
+
+        $tutorials = $tutorialRepository->filterByTheme($searchData, $theme->getID());
+
+        return $this->render('search/index.html.twig', [
+            'searchData' => $searchData,
+            'tutorials' => $tutorials
+        ]);
+    }
+
+    #[Route('/search/tag/{tag}', name: 'app_search_filter_tag')]
+    public function filterByTag(
+        Request $request,
+        Tag $tag,
+        TutorialRepository $tutorialRepository
+    ): Response {
+        $session = $request->getSession();
+        $session->set("tag", $tag->getTitle());
+        $searchData = $session->get("searchData");
+
+        $tutorials = $tutorialRepository->filterByTag($searchData, $tag->getID());
+
+        return $this->render('search/index.html.twig', [
+            'searchData' => $searchData,
+            'tutorials' => $tutorials
         ]);
     }
 }
--- src/Repository/TutorialRepository.php
+++ src/Repository/TutorialRepository.php
@@ -2,9 +2,9 @@
 
 namespace App\Repository;
 
+use App\Entity\Theme;
 use App\Entity\Tutorial;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\ORM\EntityManager;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -17,7 +17,7 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class TutorialRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(ManagerRegistry $registry,)
     {
         parent::__construct($registry, Tutorial::class);
     }
@@ -40,30 +40,30 @@ class TutorialRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Tutorial[] Returns an array of Tutorial objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('t.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    //    /**
+    //     * @return Tutorial[] Returns an array of Tutorial objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('t')
+    //            ->andWhere('t.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('t.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
 
-//    public function findOneBySomeField($value): ?Tutorial
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    public function findOneBySomeField($value): ?Tutorial
+    //    {
+    //        return $this->createQueryBuilder('t')
+    //            ->andWhere('t.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 
     public function searchTutorials(string $searchData): array
     {
@@ -72,4 +72,72 @@ class TutorialRepository extends ServiceEntityRepository
             ->getQuery()
             ->getResult();
     }
+
+    public function filterByCategory(string $searchData, int $categoryID): array
+    {
+        $qb = $this->createQueryBuilder("tutorial")
+            ->leftJoin("tutorial.theme", "theme")
+            ->innerJoin("theme.category", "category", 'WITH', "category.id = theme.category")
+            ->andWhere("tutorial.title LIKE :searchData")
+            ->setParameter("searchData", "%" . $searchData . "%")
+            ->andWhere("theme.id = " . $categoryID);
+
+        return $qb->getQuery()->execute();
+    }
+
+    public function filterByTheme(string $searchData, int $themeID): array
+    {
+        $qb = $this->createQueryBuilder("tutorial")
+            ->leftJoin("tutorial.theme", "theme")
+            ->andWhere("tutorial.title LIKE :searchData")
+            ->setParameter("searchData", "%" . $searchData . "%")
+            ->andWhere("theme.id = " . $themeID);
+
+        return $qb->getQuery()->execute();
+    }
+
+
+    public function filterByTag(string $searchData, int $tagsID): array
+    {
+        $qb = $this->createQueryBuilder("tutorial")
+            ->leftJoin("tutorial.tags", "tags")
+            ->andWhere("tutorial.title LIKE '%" . $searchData . "%'")
+            ->andWhere("tags.id = " . $tagsID);
+
+        return $qb->getQuery()->execute();
+    }
+
+
+
+    // WIP
+    // public function filterBy(string $searchData, ?int $categoryID, ?int $themeID, ?array $tagIDs)
+    // {
+    //     $qb = $this->createQueryBuilder("tutorial")
+    //         ->where("tutorial.title LIKE '%vol%'");
+
+    //     if ($categoryID) {
+    //         $qb
+    //             ->innerJoin(Category::class, "category", 'WITH', "category = tutorial.theme.category")
+    //             ->andWhere("category.id = " . $categoryID);
+    //     }
+
+    //     if ($themeID) {
+    //         $qb
+    //             ->innerJoin("tutorial.theme", "theme", 'WITH', "theme = tutorial.theme")
+    //             ->andWhere("theme.id = " . $themeID);
+    //     }
+
+    //     if ($tagIDs) {
+    //         $qb->innerJoin("tutorial.tags", "tags", 'WITH', "tags.id = tutorial.tags");
+
+    //         foreach ($tagIDs as $tagID) {
+    //             $qb->andWhere("tags.id = " . $tagID);
+    //         }
+    //     }
+
+    //     // dd($qb->getQuery());
+    //     dd($qb->getQuery()->execute());
+
+    //     // return $qb->getQuery()->execute();
+    // }
 }
