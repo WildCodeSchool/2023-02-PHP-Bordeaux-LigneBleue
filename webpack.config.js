1e6b46960d8456ac556f248b9acedbbb9fe3953a
Garcon2Jeu <104232891+Garcon2Jeu@users.noreply.github.com>
2023-06-26 13:59:43 +0200

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -70,17 +70,17 @@ Encore
     // uncomment if you use TypeScript
     // .enableTypeScriptLoader()
 
-    // uncomment if you use React
-    // .enableReactPreset()
+// uncomment if you use React
+// .enableReactPreset()
 
-    // uncomment to get integrity="..." attributes on your script & link tags
-    // requires WebpackEncoreBundle 1.4 or higher
-    // .enableIntegrityHashes(Encore.isProduction())
+// uncomment to get integrity="..." attributes on your script & link tags
+// requires WebpackEncoreBundle 1.4 or higher
+// .enableIntegrityHashes(Encore.isProduction())
 
-    // uncomment if you're having problems with a jQuery plugin
-    // .autoProvidejQuery()
+// uncomment if you're having problems with a jQuery plugin
+// .autoProvidejQuery()
 
-    // enables Sass/SCSS support
+// enables Sass/SCSS support
     .enableSassLoader();
 const fullConfig = Encore.getWebpackConfig();
 fullConfig.devServer = {
