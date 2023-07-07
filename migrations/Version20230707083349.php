0018c7675b761c29b7aba244b50da8452e3cc705
Garcon2Jeu <104232891+Garcon2Jeu@users.noreply.github.com>
2023-07-07 10:35:30 +0200

    Finished search bar

--- migrations/Version20230707083349.php
+++ migrations/Version20230707083349.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230705133052 extends AbstractMigration
+final class Version20230707083349 extends AbstractMigration
 {
     public function getDescription(): string
     {
