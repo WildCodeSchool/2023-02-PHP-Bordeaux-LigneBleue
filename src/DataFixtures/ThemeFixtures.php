e484ff706c6a0cec59730f08cb184e9be8fcf71e
Garcon2Jeu <104232891+Garcon2Jeu@users.noreply.github.com>
2023-07-24 12:30:47 +0200

    Updated Theme Fixtures
    Also added a bunch of icons

--- src/DataFixtures/ThemeFixtures.php
+++ src/DataFixtures/ThemeFixtures.php
@@ -5,50 +5,94 @@ namespace App\DataFixtures;
 use App\Entity\Theme;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Faker\Factory;
-use Faker\Generator;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ThemeFixtures extends Fixture implements DependentFixtureInterface
 {
-    private Generator $faker;
     private SluggerInterface $slugger;
 
     public function __construct(SluggerInterface $slugger)
     {
-        $this->faker = Factory::create();
         $this->slugger = $slugger;
     }
 
     public function load(ObjectManager $manager): void
     {
-        $themesAmount = 12;
+        $manager->persist($this->feedThemeObject(
+            "Communication",
+            1,
+            "smartphone/communications/llb-icon-message.png",
+            "category_Smartphone",
+            'Smartphone'
+        ));
 
-        for ($i = 1; $i <= $themesAmount; $i++) {
-            $theme = new Theme();
-            $themeTitle = $this->faker->words(2, true);
-            $theme->setTitle($themeTitle);
-            $theme->setIndexOrder($i + 1);
-            $slug = $this->slugger->slug($themeTitle);
-            $theme->setSlug($slug);
+        $manager->persist($this->feedThemeObject(
+            "Hardware",
+            2,
+            "smartphone/hardware/llb-icon-hardware.png",
+            "category_Smartphone",
+            'Smartphone'
+        ));
 
-            $this->addReference("theme_" . $theme->getTitle(), $theme);
+        $manager->persist($this->feedThemeObject(
+            "Software",
+            3,
+            "smartphone/software/llb-icon-software.png",
+            "category_Smartphone",
+            'Smartphone'
+        ));
 
-            if ($i < 4) {
-                $theme->setCategory($this->getReference("category_Smartphone"));
-                $theme->setIconPath("IconPhone.png");
-            } elseif ($i > 8) {
-                $theme->setCategory($this->getReference("category_Ordinateur"));
-                $theme->setIconPath("IconComputer.png");
-            } else {
-                $theme->setCategory($this->getReference("category_Autres"));
-                $theme->setIconPath("IconPrinter.png");
-            }
 
-            $this->addReference("theme_" . $i, $theme);
-            $manager->persist($theme);
-        }
+        $manager->persist($this->feedThemeObject(
+            "Hardware",
+            1,
+            "desktop/hardware/llb-icon-desktop.png",
+            "category_Ordinateur",
+            'Computer'
+        ));
+
+        $manager->persist($this->feedThemeObject(
+            "Internet",
+            2,
+            "desktop/browser/llb-icon-browser.png",
+            "category_Ordinateur",
+            'Computer'
+        ));
+
+        $manager->persist($this->feedThemeObject(
+            "Communications",
+            3,
+            "desktop/communications/llb-icon-paperPlane.png",
+            "category_Ordinateur",
+            'Computer'
+        ));
+
+
+        $manager->persist($this->feedThemeObject(
+            "Accessoires Connectés",
+            1,
+            "others/accessories/llb-icon-watch.png",
+            "category_Autres",
+            'Others'
+        ));
+
+        $manager->persist($this->feedThemeObject(
+            "Box Internet",
+            2,
+            "others/boxInternet/llb-icon-server.png",
+            "category_Autres",
+            'Others'
+        ));
+
+        $manager->persist($this->feedThemeObject(
+            "TVs Connectées",
+            3,
+            "others/smartTvs/llb-icon-smartTV.png",
+            "category_Autres",
+            'Others'
+        ));
+
 
         $manager->flush();
     }
@@ -56,5 +100,24 @@ class ThemeFixtures extends Fixture implements DependentFixtureInterface
     public function getDependencies()
     {
         return [CategoryFixtures::class];
+    }
+
+    public function feedThemeObject(
+        string $title,
+        int $indexOrder,
+        string $iconPath,
+        string $categoryRef,
+        string $themeRef
+    ): Theme {
+        $theme = new Theme();
+
+        $theme->setTitle($title);
+        $theme->setIndexOrder($indexOrder);
+        $theme->setSlug($this->slugger->slug($theme->getTitle()));
+        $theme->setIconPath($iconPath);
+        $theme->setCategory($this->getReference($categoryRef));
+        $this->addReference("theme_" . $themeRef . "_" . $theme->getTitle(), $theme);
+
+        return $theme;
     }
 }
