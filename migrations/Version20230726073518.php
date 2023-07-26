f02002ccd8bceb5e10f8aa17289ac60ec8d7db5c
ETIENNE_JOUAULT <etienne.jouault@live.fr>
2023-07-26 10:47:47 +0200

    add fixture picture

--- migrations/Version20230726073518.php
+++ migrations/Version20230726073518.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230725072318 extends AbstractMigration
+final class Version20230726073518 extends AbstractMigration
 {
     public function getDescription(): string
     {
